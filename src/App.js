65f160da1cc5c4a9d0fafaf5556182ede818b416
Fresh <caioneresf@gmail.com>
2022-10-13 16:41:57 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ try{
 const  response = await api_cep.get(input+'/json');
 setCep(response.data)
 } catch {
-alert("Oops... Ocorreu um erro, tente novamente mais tarde!")
+alert("Oops... Nada encontrado, verifique se o cep está correto!")
 setInput("")
 }
   }
@@ -40,12 +40,18 @@ required
 
 {Object.keys(cep).length > 0 && (
     <main className='main'>
-      <h2>CEP: {cep.cep}</h2>
+      <h2>Informações</h2>
 
+      <span>CEP: {cep.cep}</span>
       <span>{cep.logradouro}</span>
-      <span>Complemento: {cep.complemento}</span>
+      <span>Complemento: {cep.complemento == "" ? "Nenhum" : cep.complemento}</span>
       <span>Bairro: {cep.bairro}</span>
       <span>{cep.localidade} - {cep.uf}</span>
+      <span>━━━━━━━━━━━━━━━━━━━━━</span>
+      <span>IBGE: {cep.ibge != "" ? cep.ibge : "Nada Encontrado"}</span>
+      <span>GIA: {cep.gia != "" ? cep.gia : "Nada Encontrado"}</span>
+      <span>DDD: {cep.ddd}</span>
+      <span>SIAFI: {cep.siafi}</span>
     </main>
 )}
 </div>
